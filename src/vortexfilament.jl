0b37289279dddcd3dc77a71c2be6e113a93fc356
Diederik Beckers <beckersdiederik@gmail.com>
2021-05-05 13:52:50 -0700

    Removed assertions and improved implementation

--- src/vortexfilament.jl
+++ src/vortexfilament.jl
@@ -3,7 +3,7 @@ using RecipesBase
 import StaticArrays: SVector
 import LinearAlgebra: norm, dot, cross
 
-export VortexFilament, Segment, getfreevertices, inducevelocity
+export VortexFilament, Segment, getfreevertices, inducevelocity, issemiinf
 
 const sigma = 0.005;
 const sigsq = sigma*sigma;
@@ -18,11 +18,11 @@ Defines a vortex filament of strength `Γ` consisting segments that connect the 
 
 $(TYPEDFIELDS)
 """
-struct VortexFilament
+struct VortexFilament{T<:AbstractVector}
     """Γ: Strength of the vortex filament."""
     Γ::Real
     """vertices: Array of vertices of the vortex filament."""
-    vertices::Vector{AbstractVector}
+    vertices::Vector{T}
     """segments: Array of segments of the vortex filament."""
     segments::Vector{Segment}
     """freeidx: Array of indices of the free vertices of the vortex filament."""
@@ -31,23 +31,8 @@ struct VortexFilament
     boundidx::Vector{Int}
 end
 
-    function VortexFilament(Γ::Real, vertices::Vector{<:AbstractVector}, boundidx::Vector{Int}=Int64[])
+function VortexFilament(Γ::Real, vertices::Vector{T}, boundidx::Vector{Int}=Int64[]) where {T<:AbstractVector}
     infbools = map(v->in(Inf,abs.(v)),vertices) # boolean array with the i-th element true if the i-th element of vertices lies at infinity
-    @assert length(vertices) > 1 "The vortex filament has to contain at least two vertices."
-    @assert all(length.(vertices) .== 3) "Each vertex should have three coordinates."
-    @assert all(length.(findall.(isinf,vertices)) .< 2) "A vertex cannot have more than one infinite coordinate value."
-    @assert !in(true,infbools[2:end-1]) "Only the first or last vertex can have an infinite coordinate value."
-    @assert !(in(1,boundidx) && infbools[1]) "The first vertex lies at infinity and can therefore not be a bound vertex."
-    @assert !(in(length(vertices),boundidx) && infbools[end]) "The last vertex lies at infinity and can therefore not be a bound vertex."
-    if length(vertices) == 2 && all(infbools)
-        dir1 = findnext(isinf,vertices[1],1)
-        dir2 = findnext(isinf,vertices[2],1)
-        @assert (dir1 == dir2 && vertices[1][dir1] == -vertices[2][dir2]) "A filament with just two vertices, both at infinity, should have its vertices at minus and plus infinity in the same direction."
-    end
-
-    if vertices[1] == vertices[end] # if the first and last vertex in vertices are equal, remove the last one
-        pop!(vertices)
-    end
 
     segments = Segment.(vertices[1:end-1],vertices[2:end])
 
@@ -55,36 +40,97 @@ end
         push!(segments,Segment(vertices[end],vertices[1]))
     end
 
-    if infbools[1] && !infbools[end] # if either the first or last vertex lies at infinity and the other does not, the other vertex has to be a bound vertex
-        push!(boundidx,length(vertices))
-    elseif !infbools[1] && infbools[end]
-        push!(boundidx,1)
-    end
-
     boundidx = unique(boundidx) # remove any duplicates from boundidx
     freeidx = setdiff(1:length(vertices),boundidx)
 
-    return VortexFilament(Γ,vertices,segments,freeidx,boundidx)
+    return VortexFilament{T}(Γ,vertices,segments,freeidx,boundidx)
 end
 
 """
 $(TYPEDSIGNATURES)
 
-Returns the vertices of the vortex filament `vf` that are free.
+Checks if `vf` is an infinite vortex filament.
 """
-function getfreevertices(vf::VortexFilament)
-    vf.vertices[vf.freeidx]
+function Base.:isinf(vf::VortexFilament)
+    any(isinf,vf.segments) && return true
+    semiinfsegs = findall(issemiinf,vf.segments)
+    return length(semiinfsegs) > 1
 end
 
 """
 $(TYPEDSIGNATURES)
 
-Computes the induced velocity by one segment `s` of a unit-strength vortex filament on the evaluation point `xeval`.
+Checks if `s` is an infinite segment.
+"""
+function Base.:isinf(s::Segment)
+    return in(Inf,abs.(s[1])) && in(Inf,abs.(s[2]))
+end
+
+"""
+$(TYPEDSIGNATURES)
+
+Checks if `vf` is a semi-infinite vortex filament.
+"""
+function issemiinf(vf::VortexFilament)
+    semiinfsegs = findall(issemiinf,vf.segments)
+    return length(semiinfsegs) == 1
+end
+
+"""
+$(TYPEDSIGNATURES)
+
+Checks if `s` is a semi-infinite segment.
+"""
+function issemiinf(s::Segment)
+    return xor(in(Inf,abs.(s[1])),in(Inf,abs.(s[2])))
+end
+
+"""
+$(TYPEDSIGNATURES)
+
+Returns the index of the coordinate for which the first and/or last vertex of the vortex filament `vf` has an infinite value if it has one.
+"""
+function infdir(vf::VortexFilament)
+    if isinf(vf) || issemiinf(vf)
+        segdirs = infdir.(vf.segments)
+        uniquesegdirs = unique(filter!(dir->dir≠nothing,segdirs))
+        length(uniquesegdirs) > 1 && throw(ErrorException("Inf values in different coordinates"))
+        return uniquesegdirs[1]
+    else
+        return nothing
+    end
+end
+
+"""
+$(TYPEDSIGNATURES)
+
+Returns the index of the coordinate for which the first and/or second vertex of the segment `s` has an infinite value if it has one.
+"""
+function infdir(s::Segment)
+    !all(length.(findall.(isinf,s)) .< 2) && throw(ErrorException("Inf values in different coordinates"))
+    if issemiinf(s)
+        Inf in abs.(s[1]) ? infidx = 1 : infidx = 2
+        return findnext(isinf,s[infidx],1)
+    elseif isinf(s)
+        dir1 = findnext(isinf,s[1],1)
+        dir2 = findnext(isinf,s[2],1)
+        dir1 == dir2 ? dir = dir1 : throw(ErrorException("Inf values in different coordinates"))
+        sign(s[1][dir]) == sign(s[2][dir]) && throw(ErrorException("Infinite segment has non-opposite infinite vertices"))
+        return dir
+    else
+        return nothing
+    end
+end
+
+"""
+$(TYPEDSIGNATURES)
+
+Computes the induced velocity by one segment `s` of a unit-strength vortex filament on the evaluation point `xeval`. In case both vertices of the segment have an infinite coordinate (which have to be in the same direction), then the non-infinite coordinates are averaged over the two vertices to calculate the distance from xeval to the segment.
 """
 function inducevelocity(s::Segment,xeval)
     if s[1] == s[2]
         return zeros(3)
-    elseif xor(in(Inf,abs.(s[1])),in(Inf,abs.(s[2])))
+    elseif issemiinf(s)
         #=
         In this case we calculate the induced velocity by splitting the semi-infinite segment into two segments using the point `v`, which is the closest point to xeval on the axis of the semi-infinite segment. At the end we sum the velocities induced by the two segments using two multipliers `K1` and `K2` to determine the sign of the velocities.
 
@@ -96,7 +142,7 @@ function inducevelocity(s::Segment,xeval)
         infidx = findnext(v->in(Inf,abs.(v)),s,1)
         infidx == 1 ? (finidx = 2; K1 = -1) : (finidx = 1; K1 = 1)
 
-        dir = findnext(isinf,s[infidx],1)
+        dir = infdir(s)
         edir = zeros(3)
         edir[dir] = sign(s[infidx][dir])
 
@@ -108,8 +154,8 @@ function inducevelocity(s::Segment,xeval)
         vnet = K1*K2*vfiniteseg + K1*vseminfseg
         return vnet
 
-    elseif in(Inf,abs.(s[1])) && in(Inf,abs.(s[2]))
-        dir = findnext(isinf,s[1],1)
+    elseif isinf(s)
+        dir = infdir(s)
         edir = zeros(3)
         edir[dir] = sign(s[2][dir])
 
@@ -154,22 +200,6 @@ function inducevelocity(vf::VortexFilament,xeval)
     vel *= vf.Γ
 end
 
-function _closestpointonsemiinfinitesegment(s::Segment,p)
-    infidx = findnext(v->in(Inf,abs.(v)),s,1)
-    infidx == 1 ? finidx = 2 : finidx = 1
-    dir = findnext(isinf,s[infidx],1)
-    a = s[finidx]
-    segdir = zeros(3)
-    segdir[dir] = sign(s[infidx][dir])
-    ap = p-a
-    t = dot(ap,segdir)
-    if t < 0
-        return a # p lies further than the segment, so the finite endpoint of the segment is the closest point.
-    else
-        return a + t*segdir
-    end
-end
-
 function _closestpointonfinitesegmentaxis(s::Segment,p)
     a = s[1]
     b = s[2]
@@ -179,6 +209,56 @@ function _closestpointonfinitesegmentaxis(s::Segment,p)
     return a + t*ab, t
 end
 
-@recipe f(vf::VortexFilament) = map(p->p.x, vcat(vf.vertices,[vf.vertices[1]])), map(p->p.y, vcat(vf.vertices,[vf.vertices[1]])), map(p->p.z, vcat(vf.vertices,[vf.vertices[1]]))
+lims(dir) = (:xlims, :ylims, :zlims)[dir]
+
+@recipe function f(vf::VortexFilament)
+    segments = vf.segments
+    if isinf(vf) || issemiinf(vf)
+        segments = deepcopy(vf.segments)
+        for s in segments
+            if isinf(s) || issemiinf(s)
+                inftydir = infdir(s)
+                noninftydir = filter!(x->x≠inftydir,[1,2,3])
+                inflims = get(plotattributes,lims(inftydir),:none)
+                inflims == :none && throw(ArgumentError("The filament has a vertex at infinity, please provide axis limits"))
+                if isinf(s)
+                    centerpoint = 0.5*(s[1] + s[2])
+                    s[1][noninftydir] .= centerpoint[noninftydir]
+                    s[2][noninftydir] .= centerpoint[noninftydir]
+                end
+                if isinf(s[1][inftydir])
+                    sign(s[1][inftydir]) == -1 ? limitidx = 1 : limitidx = 2
+                    s[1][inftydir] = inflims[limitidx]
+                    s[1][noninftydir] .= s[2][noninftydir]
+                end
+                if isinf(s[2][inftydir])
+                    sign(s[2][inftydir]) == -1 ? limitidx = 1 : limitidx = 2
+                    s[2][inftydir] = inflims[limitidx]
+                    s[2][noninftydir] .= s[1][noninftydir]
+                end
+            end
+        end
+
+        # vertices = deepcopy(vcat((s->s[1]).(vf.segments),[vf.segments[end][end]]))
+        #
+        # if in(Inf,abs.(vf.vertices[1])) && in(Inf,abs.(vf.vertices[2]))
+        #     centerpoint = 0.5*(vf.vertices[1] + vf.vertices[2])
+        #     vf.vertices[1][filter!(x->x≠dir,[1,2,3])] .= centerpoint[filter!(x->x≠dir,[1,2,3])]
+        #     vf.vertices[2][filter!(x->x≠dir,[1,2,3])] .= centerpoint[filter!(x->x≠dir,[1,2,3])]
+        # end
+        #
+        # for t in [(1,2),(length(vertices),length(vertices)-1)]
+        #     if -Inf in vertices[t[1]][dir]
+        #         vertices[t[1]] .= vertices[t[2]]
+        #         vertices[t[1]][dir] = inflims[1]
+        #     elseif Inf in vertices[t[1]][dir]
+        #         vertices[t[1]] .= vertices[t[2]]
+        #         vertices[t[1]][dir] = inflims[2]
+        #     end
+        # end
+    end
+    vertices = vcat((s->s[1]).(segments),[segments[end][end]])
+    return (v->v[1]).(vertices), (v->v[2]).(vertices), (v->v[3]).(vertices)
+end
 
 Base.:(==)(a::VortexFilament, b::VortexFilament) = isequal(a.Γ, b.Γ) && isequal(a.vertices, b.vertices) &&  isequal(a.segments, b.segments) && isequal(a.freeidx, b.freeidx) && isequal(a.boundidx, b.boundidx)
--- test/vortexfilament.jl
+++ test/vortexfilament.jl
@@ -5,77 +5,44 @@ using LinearAlgebra: norm
     v1 = rand(3)
     v2 = rand(3)
     v3 = rand(3)
-    vtwodim = rand(2)
-    vtwoinf = [rand(Float64), Inf, Inf]
-    vmininfx = [rand(Float64), rand(Float64), -Inf]
-    vplusinfx = [rand(Float64), rand(Float64), Inf]
-    vplusinfx2 = [rand(Float64), rand(Float64), Inf]
-    vplusinfy = [rand(Float64), Inf, rand(Float64)]
 
-    @test_throws AssertionError VortexFilament(1.0,[v1])
-    @test_throws AssertionError VortexFilament(1.0,[v1,vtwodim,v3])
-    @test_throws AssertionError VortexFilament(1.0,[v1,vtwoinf,v3])
-    @test_throws AssertionError VortexFilament(1.0,[vplusinfx,vplusinfy])
-    @test_throws AssertionError VortexFilament(1.0,[vplusinfx,vplusinfx2])
-    @test VortexFilament(1.0,[vmininfx,vplusinfx]) == VortexFilament(1.0,[vmininfx,vplusinfx],[Segment(vmininfx,vplusinfx)],[1,2],Int64[])
-    @test VortexFilament(1.0,[vplusinfx,vmininfx]) == VortexFilament(1.0,[vplusinfx,vmininfx],[Segment(vplusinfx,vmininfx)],[1,2],Int64[])
     @test VortexFilament(1.0,[v1,v2]) == VortexFilament(1.0,[v1,v2],[Segment(v1,v2),Segment(v2,v1)],[1,2],Int64[])
     @test VortexFilament(1.0,[v1,v2,v3]) == VortexFilament(1.0,[v1,v2,v3],[Segment(v1,v2),Segment(v2,v3),Segment(v3,v1)],[1,2,3],Int64[])
     @test VortexFilament(1.0,[v1,v2,v3],[2]) == VortexFilament(1.0,[v1,v2,v3],[Segment(v1,v2),Segment(v2,v3),Segment(v3,v1)],[1,3],[2])
-    @test VortexFilament(1.0,[v1,v2,v3,v1]) == VortexFilament(1.0,[v1,v2,v3],[Segment(v1,v2),Segment(v2,v3),Segment(v3,v1)],[1,2,3],Int64[])
-
-    r1 = rand(2)
-    r2 = rand(2)
-    for vinf1 in [[Inf,r1[1],r1[2]],[-Inf,r1[1],r1[2]],[r1[1],Inf,r1[2]],[r1[1],-Inf,r1[2]],[r1[1],r1[2],Inf],[r1[1],r1[2],-Inf]]
-        @test VortexFilament(1.0,[v1,vinf1]) == VortexFilament(1.0,[v1,vinf1],[Segment(v1,vinf1)],[2],[1])
-        @test VortexFilament(1.0,[vinf1,v1]) == VortexFilament(1.0,[vinf1,v1],[Segment(vinf1,v1)],[1],[2])
-        @test VortexFilament(1.0,[v1,v2,vinf1]) == VortexFilament(1.0,[v1,v2,vinf1],[Segment(v1,v2),Segment(v2,vinf1)],[2,3],[1])
-        @test VortexFilament(1.0,[vinf1,v1,v2]) == VortexFilament(1.0,[vinf1,v1,v2],[Segment(vinf1,v1),Segment(v1,v2)],[1,2],[3])
-        @test_throws AssertionError VortexFilament(1.0,[v1,vinf1,v2])
-        @test_throws AssertionError VortexFilament(1.0,[vinf1,v1,v2],[1])
-        @test_throws AssertionError VortexFilament(1.0,[v1,v2,vinf1],[3])
-        for vinf2 in [[Inf,r2[1],r2[2]],[-Inf,r2[1],r2[2]],[r2[1],Inf,r2[2]],[r2[1],-Inf,r2[2]],[r2[1],r2[2],Inf],[r2[1],r2[2],-Inf]]
-            @test VortexFilament(1.0,[vinf1,v1,vinf2]) == VortexFilament(1.0,[vinf1,v1,vinf2],[Segment(vinf1,v1),Segment(v1,vinf2)],[1,2,3],Int64[])
-            @test_throws AssertionError VortexFilament(1.0,[vinf1,v1,vinf2],[1])
-            @test_throws AssertionError VortexFilament(1.0,[vinf1,v1,vinf2],[3])
-            @test_throws AssertionError VortexFilament(1.0,[vinf1,v1,vinf2],[1,3])
-        end
-    end
 end
 
-@testset "_closestpointonsemiinfinitesegment" begin
-    vfin = rand(3)
-    pplus = vfin + rand(3)
-    pmin = vfin - rand(3)
-    for infdir in 1:3
-        vinf = rand(3)
-        vinf[infdir] = Inf
+@testset "isinf" begin
+    v1 = [Inf,-1,0]
+    v2 = [0,-1,0]
+    v3 = [0,1,0]
+    v4 = [Inf,1,0]
+    vf = VortexFilament(1,[v1,v2,v3,v4])
+    @test isinf(vf)
 
-        s = Segment(vfin,vinf)
-        closestpoint = vfin
-        closestpoint[infdir] = pplus[infdir]
-        @test VortexFilaments._closestpointonsemiinfinitesegment(s,pplus) == closestpoint
-        @test VortexFilaments._closestpointonsemiinfinitesegment(s,pmin) == vfin
+    v1 = [-Inf,1,0]
+    v2 = [Inf,1,0]
+    vf = VortexFilament(1,[v1,v2])
+    @test isinf(vf)
+end
 
-        s = Segment(vinf,vfin)
-        closestpoint = vfin
-        closestpoint[infdir] = pplus[infdir]
-        @test VortexFilaments._closestpointonsemiinfinitesegment(s,pplus) == closestpoint
-        @test VortexFilaments._closestpointonsemiinfinitesegment(s,pmin) == vfin
+@testset "issemiinf" begin
+    v1 = rand(3)
+    v2 = [Inf,rand(Float64),rand(Float64)]
+    vf = VortexFilament(1,[v1,v2])
+    @test issemiinf(vf)
 
-        s = Segment(vfin,-vinf)
-        closestpoint = vfin
-        closestpoint[infdir] = pmin[infdir]
-        @test VortexFilaments._closestpointonsemiinfinitesegment(s,pmin) == closestpoint
-        @test VortexFilaments._closestpointonsemiinfinitesegment(s,pplus) == vfin
+    v1 = [rand(Float64),-Inf,rand(Float64)]
+    v2 = rand(3)
+    v3 = rand(3)
+    vf = VortexFilament(1,[v1,v2,v3])
+    @test issemiinf(vf)
 
-        s = Segment(-vinf,vfin)
-        closestpoint = vfin
-        closestpoint[infdir] = pmin[infdir]
-        @test VortexFilaments._closestpointonsemiinfinitesegment(s,pmin) == closestpoint
-        @test VortexFilaments._closestpointonsemiinfinitesegment(s,pplus) == vfin
-    end
-
+    v1 = [Inf,-1,0]
+    v2 = rand(3)
+    v3 = rand(3)
+    v4 = [Inf,1,0]
+    vf = VortexFilament(1,[v1,v2,v3,v4])
+    @test issemiinf(vf) == false
 end
 
 @testset "_closestpointonfinitesegmentaxis" begin
@@ -85,16 +52,41 @@ end
     @test VortexFilaments._closestpointonfinitesegmentaxis(Segment(v1,v2),[-2,1,0]) == ([-2,0,0],-2)
 end
 
+@testset "infdir" begin
+    v1 = rand(3)
+    v2 = rand(3)
+    v3 = rand(3)
+    vtwoinf = [rand(Float64), Inf, Inf]
+    vplusinfx = [Inf,rand(Float64),rand(Float64)]
+    vplusinfy = [rand(Float64),Inf,rand(Float64)]
+    vplusinfz = [rand(Float64),rand(Float64),Inf]
+    vmininfx = [-Inf,rand(Float64),rand(Float64)]
+    vmininfy = [rand(Float64),-Inf,rand(Float64)]
+    vmininfz = [rand(Float64),rand(Float64),-Inf]
+    @test_throws ErrorException VortexFilaments.infdir(VortexFilament(1.0,[v1,v2,vtwoinf]))
+    @test_throws ErrorException VortexFilaments.infdir(VortexFilament(1.0,[vplusinfx,v2,vplusinfy]))
+    @test VortexFilaments.infdir(VortexFilament(1.0,[v1,v2,vplusinfx])) == 1
+    @test VortexFilaments.infdir(VortexFilament(1.0,[v1,v2,vplusinfy])) == 2
+    @test VortexFilaments.infdir(VortexFilament(1.0,[v1,v2,vplusinfz])) == 3
+    @test VortexFilaments.infdir(VortexFilament(1.0,[v1,v2,vmininfx])) == 1
+    @test VortexFilaments.infdir(VortexFilament(1.0,[v1,v2,vmininfy])) == 2
+    @test VortexFilaments.infdir(VortexFilament(1.0,[v1,v2,vmininfz])) == 3
+    @test VortexFilaments.infdir(VortexFilament(1.0,[vplusinfx,v1,v2,vmininfx])) == 1
+    @test VortexFilaments.infdir(VortexFilament(1.0,[vplusinfx,v1,v2,vplusinfx])) == 1
+end
+
 @testset "inducevelocity" begin
     v0 = [0,0,0]
-    vplusinf = [Inf,0,0]
-    vminusinf = [-Inf,0,0]
+    vplusinfx = [Inf,rand(Float64),rand(Float64)]
+    vmininfx = [-Inf,rand(Float64),rand(Float64)]
+    vplusinfx2 = [Inf,rand(Float64),rand(Float64)]
+    vplusinfy = [rand(Float64),Inf,rand(Float64)]
     v1 = [-1,-1,0]
     v2 = [-1,1,0]
     v3 = [1,1,1]
     v4 = [1,-1,1]
     r = rand(Float64) + 1
-    r3 = rand(3) .+ 1
+    rvec = rand(3) .+ 1
 
     # Special cases for evaluation point
     s = Segment(v0,v3)
@@ -102,17 +94,20 @@ end
     @test inducevelocity(s,v0) == [0.0,0.0,0.0] # evaluation point on vertex 1
     @test inducevelocity(s,v3) == [0.0,0.0,0.0] # evaluation point on vertex 2
     @test inducevelocity(s,v0+(1+r)*v3) == [0.0,0.0,0.0] # evaluation point on segment axis but not on segment itself
-    s = Segment(v0,vplusinf) # semi-infinite segment
-    @test inducevelocity(s,[1,0,0]) == [0.0,0.0,0.0] # evaluation point on segment
-    @test inducevelocity(s,[-1,0,0]) == [0.0,0.0,0.0] # evaluation point on segment axis but not on segment itself
-    s = Segment(vminusinf,vplusinf) # infinite segment
-    @test inducevelocity(s,[1,0,0]) == [0.0,0.0,0.0] # evaluation point on segment
+    @test inducevelocity(Segment([0,1,2],[Inf,1,2]),[1,1,2]) == [0.0,0.0,0.0] # evaluation point on semi-infinite segment
+    @test inducevelocity(Segment([0,1,2],[Inf,1,2]),[-1,1,2]) == [0.0,0.0,0.0] # evaluation point on semi-infinite segment axis but not on segment itself
+    @test inducevelocity(Segment([-Inf,1,2],[Inf,1,2]),[1,1,2]) == [0.0,0.0,0.0] # evaluation point on infinite segment
 
     # Special segments
-    s = Segment(v0,vplusinf) # semi-infinite segment
-    @test isapprox(norm(inducevelocity(s,[0.0,r3[2],r3[3]])), 1/(4π*norm([r3[2],r3[3]])), atol=1e-3)
-    s = Segment(vminusinf,vplusinf) # infinite segment
-    @test isapprox(norm(inducevelocity(s,r3)), 1/(2π*norm([r3[2],r3[3]])), atol=1e-3)
+    s = Segment(v0,vplusinfx) # semi-infinite segment
+    @test isapprox(norm(inducevelocity(s,[0.0,rvec[2],rvec[3]])), 1/(4π*norm([rvec[2],rvec[3]])), atol=1e-3)
+    s = Segment(vmininfx,vplusinfx) # infinite segment
+    centerpoint = 0.5*(vplusinfx + vmininfx)
+    @test isapprox(norm(inducevelocity(s,rvec)), 1/(2π*norm([rvec[2]-centerpoint[2],rvec[3]-centerpoint[3]])), atol=1e-3)
+    s = Segment(vmininfx,vplusinfy) # infinite segment with different infinite coordinates
+    @test_throws ErrorException inducevelocity(s,rvec)
+    s = Segment(vplusinfx,vplusinfx2) # infinite segment with non-opposite infinite vertices
+    @test_throws ErrorException inducevelocity(s,rvec)
 
     # Horseshoe vortex
     b = rand(Float64)+1
@@ -136,6 +131,9 @@ end
     vf = VortexFilament(1.0,[v1,v2,v3,v4])
     @test inducevelocity(vf,[-1,0,1]) ≈ [0.1566735365948611; 0.0; -0.12386284345966522]
 
-
+    v1 = rand(3)
+    v2 = rand(3)
+    vtwodim = rand(2)
+    @test_throws DimensionMismatch inducevelocity(VortexFilament(1.0,[v1,vtwodim,v2]),rand(3))
 
 end
